49dcc320386afdef21c4175e5d7dc661b8259142
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-10-04 20:57:34 -0400

    remove boilerplate css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Wallet from "../components/Wallet";
 import { useMetamask } from "../hooks/useMetamask";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const { state } = useMetamask();
