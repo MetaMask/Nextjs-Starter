94d20e04998980413e3e3fe69fd6df94db5e8717
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-10-11 16:50:47 -0400

    remove listeners

--- types.d.ts
+++ types.d.ts
@@ -5,7 +5,8 @@ type InjectedProviders = {
 interface Window {
   ethereum: InjectedProviders & {
     on: (...args: any[]) => void;
-    removeListener?: (...args: any[]) => void;
+    removeListener: (...args: any[]) => void;
+    removeAllListeners: (...args: any[]) => void;
     request<T = any>(args: any): Promise<T>;
   };
 }
